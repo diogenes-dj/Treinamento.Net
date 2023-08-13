5d08fe70f941777ecd6fef45c5b3e136c90ab42f
diogenes-dj <zyguifried@hotmail.com>
2023-08-13 13:19:21 -0300

    Exercícios Iniciais

---
+++ ConsoleApp1/ConsoleApp1/Models/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿namespace ExemploFundamentos.Models //posso dar o nome q eu quiser, mas q faça sentido, afinidade. organização
+{
+    public class Pessoa //Nome do arquivo ser o mesmo nome da classe
+    {
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+        private string Nome { get; set; } //atributos da classe pessoa(nome e idade)
+        private int Idade { get; set; }
+
+        private DateTime? Agencia { get; set; }
+
+        public void Apresentar()    //método ou função da classe pessoa(são as ações)
+        {
+            Console.WriteLine($"Olá! Meu nome é " +
+            $"{Nome} e tenho {Idade} anos");
+
+            /*se fosse um texto muito grande eu poderia quebrá-lo apenas visualmente e não no resultado do conteúdo usando o + $
+            
+            classe.método (parâmetro com argumentos) quebrar texto é contra barra n \n
+            */
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExemploFundamentos.Models;
+
+Pessoa pessoa1 = new Pessoa("Diogenes",30);
+
+
+
+pessoa1.Apresentar();
+
+//usar datas
+DateTime dataAtual = DateTime.Now.AddDays(5); //Adicionando 5 dias na data
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
